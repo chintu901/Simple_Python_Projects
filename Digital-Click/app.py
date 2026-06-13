836c02f7964ae9403d194d432e08e5353127e8e6
Chetan Dev <chetanranga92@gmail.com>
2026-06-13 23:44:45 +0530

    Update app.py

--- Digital-Click/app.py
+++ Digital-Click/app.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 from datetime import datetime
-import datetime
+import datetime as 
 # Appearance settings
 ctk.set_appearance_mode("dark")   # "light" or "dark"
 ctk.set_default_color_theme("blue")
